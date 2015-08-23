29d2ee333eb6d687df88cccd39198c7743e2f759
uuu1993 <634468460@qq.com>
2015-08-23 23:32:34 +0800

    add log for touch

--- app/src/main/java/com/obo/mydemos/view/moutitouch/MoutiTouchView.java
+++ app/src/main/java/com/obo/mydemos/view/moutitouch/MoutiTouchView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -37,13 +38,25 @@ public class MoutiTouchView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        Log.i(TAG, "ActionIndex:" + event.getActionIndex());
+
         switch (event.getActionMasked())
         {
             case MotionEvent.ACTION_DOWN:
+                Log.i(TAG,"down :  X:"+event.getX(0)+"  Y:"+event.getY(0));
                 moveStartPosition.x = event.getX();
                 moveStartPosition.y = event.getY();
                 break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                Log.i(TAG,"point down 0:  X:"+event.getX(0)+"  Y:"+event.getY(0));
+                Log.i(TAG,"point down 1:  X:"+event.getX(1)+"  Y:"+event.getY(1));
+                break;
+
             case MotionEvent.ACTION_MOVE:
+
+                Log.i(TAG,"point move 0:  X:"+event.getX(0)+"  Y:"+event.getY(0));
+                if (event.getPointerCount() == 2)
+                Log.i(TAG,"point move 1:  X:"+event.getX(1)+"  Y:"+event.getY(1));
                 moveEndPosition.x = event.getX();
                 moveEndPosition.y = event.getY();
                 //刷新
@@ -51,10 +64,16 @@ public class MoutiTouchView extends View {
                 break;
 
             case MotionEvent.ACTION_UP:
+                Log.i(TAG,"up :  X:"+event.getX()+"  Y:"+event.getY());
                 currrentPosition.x += (moveEndPosition.x - moveStartPosition.x);
                 currrentPosition.y += (moveEndPosition.y - moveStartPosition.y);
                 moveStartPosition.x = moveEndPosition.x;
                 moveStartPosition.y = moveEndPosition.y;
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                Log.i(TAG,"point up 0:  X:"+event.getX(0)+"  Y:"+event.getY(0));
+                Log.i(TAG,"point up 1:  X:"+event.getX(1)+"  Y:"+event.getY(1));
+
                 break;
             default:
         }
