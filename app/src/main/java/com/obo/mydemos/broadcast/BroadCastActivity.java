39ec3921325fc6796fecfb40d9a3697502e2d468
uuu1993 <634468460@qq.com>
2015-10-21 19:11:56 +0800

    add broadcastActivity

---
+++ app/src/main/java/com/obo/mydemos/broadcast/BroadCastActivity.java
@@ -0,0 +1,33 @@
+package com.obo.mydemos.broadcast;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.obo.activity.R;
+
+public class BroadCastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broad_cast);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+
+}
--- app/src/main/java/com/obo/mydemos/webview/WebViewActivity.java
+++ app/src/main/java/com/obo/mydemos/webview/WebViewActivity.java
@@ -32,4 +32,5 @@ public class WebViewActivity extends BaseActivity {
     public void onClick(View sender) {
 
     }
+
 }
