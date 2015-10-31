7cc27394434291ac3b6f5e32eb8d70f50a63c829
uuu1993 <634468460@qq.com>
2015-10-31 08:35:10 +0800

    commit

--- app/src/main/java/com/obo/mydemos/animate/AnimateActivity.java
+++ app/src/main/java/com/obo/mydemos/animate/AnimateActivity.java
@@ -15,6 +15,7 @@ public class AnimateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animate);
+        System.gc();
         ArrTest t;
     }
 }
--- app/src/main/java/com/obo/mydemos/touch/TouchActivity.java
+++ app/src/main/java/com/obo/mydemos/touch/TouchActivity.java
@@ -5,12 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.obo.activity.R;
+import com.obo.utils.network.LogUtil;
 
 public class TouchActivity extends AppCompatActivity {
 
@@ -22,6 +24,7 @@ public class TouchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_touch);
+        LogUtil.i(this, "activity onCreate");
     }
 
     public void onClick(View view)
@@ -55,5 +58,29 @@ public class TouchActivity extends AppCompatActivity {
         Toast.makeText(TouchActivity.this, whereToClick, Toast.LENGTH_SHORT).show();
     }
 
+
+//    @Override
+//    public boolean onKeyDown(int keyCode,KeyEvent keyEvent)
+//    {
+//        switch (keyCode)
+//        {
+//            case KeyEvent.KEYCODE_BACK:
+//                LogUtil.i(this,"activity finalize");
+//
+//
+//                this.finish();
+//                return true;
+//
+//        }
+//
+//        return super.onKeyDown(keyCode,keyEvent);
+//    }
+
+    @Override
+    public void finalize()
+    {
+        LogUtil.i(this,"activity finalize");
+        System.gc();
+    }
 
 }
